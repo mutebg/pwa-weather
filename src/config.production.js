5fb1ce14c02c9b1554f033f4f459263d77ac6575
Stoyan Delev <s.delev@elements.nl>
2017-11-16 20:32:06 +0100

    fix prod api url

--- src/config.production.js
+++ src/config.production.js
@@ -1,7 +1,7 @@
 module.exports = {
 	BASE_URL: 'https://willraintoday-64e72.firebaseapp.com/',
-	//	API_URL: 'https://us-central1-willraintoday-64e72.cloudfunctions.net/api/',
-	API_URL: 'http://localhost:5000/willraintoday-64e72/us-central1/api/',
+	API_URL: 'https://us-central1-willraintoday-64e72.cloudfunctions.net/api/',
+	//API_URL: 'http://localhost:5000/willraintoday-64e72/us-central1/api/',
 
 	API_VERSION: 1
 };
